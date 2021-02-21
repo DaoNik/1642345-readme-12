91ec2943297b5c12b67e2159dbfd883dfaeb055e
Dao Nik <electronicdaos@gmail.com>
2021-02-21 19:04:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -218,28 +218,45 @@ $user_name = 'Никита'; // укажите здесь ваше имя
                 [
                     'h2' => 'Игра престолов',
                     'type' => 'post-text',
-                    'text' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+                    'main' => '<p>Не могу дождаться начала финального сезона своего любимого сериала!</p>',
                     'username' => 'Владик',
                     'avatar' => 'userpic.jpg'
                 ],
                 [
                     'h2' => 'Наконец, обработал фотки!',
                     'type' => 'post-photo',
-                    'src' => 'rock-medium.jpg',
+                    'main' => '<div class="post-photo__image-wrapper">
+                    <img src="img/rock-medium.jpg" alt="Фото от пользователя" width="360" height="240">
+                </div>',
                     'username' => 'Виктор',
                     'avatar' => 'userpic-mark.jpg'
                 ],
                 [
                     'h2' => 'Моя мечта',
                     'type' => 'post-photo',
-                    'src' => 'coast-medium.jpg',
+                    'main' => '<div class="post-photo__image-wrapper">
+                    <img src="img/coast-medium.jpg" alt="Фото от пользователя" width="360" height="240">
+                </div>',
                     'username' => 'Лариса',
                     'avatar' => 'userpic.jpg'
                 ],
                 [
                     'h2' => 'Лучшие курсы',
                     'type' => 'post-link',
-                    'href' => 'www.htmlacademy.ru',
+                    'main' => '
+                    <div class="post-link__wrapper">
+                        <a class="post-link__external" href="http://" title="Перейти по ссылке">
+                            <div class="post-link__info-wrapper">
+                                <div class="post-link__icon-wrapper">
+                                    <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
+                                </div>
+                                <div class="post-link__info">
+                                    <h3>Лучшие курсы</h3>
+                                </div>
+                            </div>
+                            <span><a href="www.htmlacademy.ru"></a></span>
+                        </a>
+                    </div>',
                     'username' => 'Владик',
                     'avatar' => 'userpic.jpg'
                 ]
